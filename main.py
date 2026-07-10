271135142251be95970697aee3cdc67f7541cfd2
Developer <dev@example.com>
2026-07-10 14:05:20 +0200

    fix: only warning logs

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ logging.basicConfig(
     level=logging.INFO
 )
 logger = logging.getLogger(__name__)
+logging.getLogger('httpx').setLevel(logging.WARNING)
+logging.getLogger('httpcore').setLevel(logging.WARNING)
 
 
 def run_async(coro):
